de7c583051877950be49a468986b2b4c76cc62a1
gurori <nazarovslava124@gmail.com>
2025-11-29 00:26:08 +0300

    Configure JWT authentication, cookie policy, CORS, and Swagger middlewares

--- Program.cs
+++ Program.cs
@@ -1,11 +1,39 @@
+using Microsoft.AspNetCore.CookiePolicy;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 var services = builder.Services;
+var configuration = builder.Configuration;
+
+//services.Configure<JwtOptions>(configuration.GetSection(nameof(JwtOptions)));
+//services.Configure<AuthorizationOptions>(configuration.GetSection(nameof(AuthorizationOptions)));
 
 services.AddEndpointsApiExplorer();
 services.AddSwaggerGen();
 
+services.AddCors(option =>
+{
+    option.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins("https://localhost:3000/");
+        policy.AllowCredentials();
+        policy.AllowAnyHeader();
+        policy.AllowAnyMethod();
+    });
+});
+
+services.AddControllers();
+
+// services.AddDbContext<ItesDbContext>(options =>
+//     options.UseNpgsql(configuration.GetConnectionString(nameof(ItesDbContext)))
+// );
+
 var app = builder.Build();
+
+// using var scope = app.Services.CreateScope();
+// await using var dbContext = scope.ServiceProvider.GetRequiredService<ItesDbContext>();
+// await dbContext.Database.EnsureCreatedAsync();
 
 if (app.Environment.IsDevelopment())
 {
@@ -13,6 +41,22 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.MapGet("/", () => "Hello World!");
+app.UseHttpsRedirection();
+
+app.UseCookiePolicy(
+    new CookiePolicyOptions
+    {
+        MinimumSameSitePolicy = SameSiteMode.None,
+        HttpOnly = HttpOnlyPolicy.Always,
+        Secure = CookieSecurePolicy.Always,
+    }
+);
+
+app.UseCors();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
